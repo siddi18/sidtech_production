a065c3cc47d5ddd0023c45f320adcb8fa142fdce
siddi <crazythoughts1729@gmail.com>
2024-04-19 23:00:32 +0530

    fifth commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    proxy : {
-      '/api' : 'https://sparktech.onrender.com',
-      '/upload' : 'https://sparktech.onrender.com'
-    }
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'https://sparktech.onrender.com',
+      '/upload': 'https://sparktech.onrender.com'
+    },
   },
+  build: {
+    outDir: path.resolve(__dirname, 'dist') // Specify the correct output directory
+  }
 })
