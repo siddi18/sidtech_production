641827ce2eb4d124ba9ede7bcc845cc49d0a91ec
siddi <crazythoughts1729@gmail.com>
2024-04-24 08:41:15 +0530

    fail1 commit

--- server/routes/uploadRoute.js
+++ server/routes/uploadRoute.js
@@ -3,7 +3,7 @@ import express from "express"
 import multer from "multer"
 
 const router = express.Router()
-
+console.log('heelo')
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, "../uploads/")
