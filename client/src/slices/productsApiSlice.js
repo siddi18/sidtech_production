e28f2e7e168c535554c34ec3f754a9c51c5e48be
siddi <crazythoughts1729@gmail.com>
2024-04-24 09:00:56 +0530

    fail1 commit

--- client/src/slices/productsApiSlice.js
+++ client/src/slices/productsApiSlice.js
@@ -5,7 +5,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: ({keyword, pageNumber}) => ({
-        url: PRODUCTS_URL,
+        url: 'https://sidtech.onrender.com',
         params: {keyword, pageNumber}
       }),
       keepUnusedDataFor: 5, // Optional: Cache data for 5 minutes
